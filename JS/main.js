6e281e5ce1e339160c85767dda604703ae33d629
MirkoGLostia <giggi.torre@mail.com>
2023-04-29 20:16:35 +0200

    html_js_first-milestone

--- JS/main.js
+++ JS/main.js
@@ -9,86 +9,75 @@ let distance, utAge, utPrezzo, prezzoConEta, prezzoFinale, toPay, utName;
 const prezzoBase = 21;
 
 
-// richiesta info utente
-utName = prompt ("come ti chiami");
-
-distance = parseInt ( prompt ("quanti chilometri devi percorrere? controlla la mappa delle rotaie se non sei sicuro, e ricorda, se devi tornare devi raddoppiare la distanza"));
-
-utAge = parseInt ( prompt ("quanti anni hai?"));
-
-
-// calcolo prezzo biglietto, basato sulla distanza, in centesimi
-utPrezzo = distance * prezzoBase;
-
-
-// calcolo prezzo finale basato sull'età
-if (utAge < 18) {
-    // se minore di 18, 20% di sconto
-    const discount = (utPrezzo * 20) / 100;
-    prezzoConEta =  utPrezzo - discount;
-
-
-    //conversione centesimi in euro
-    prezzoFinale = prezzoConEta / 100;
-
-
-    // debug
-    // console.log(discount);
-    // console.log(prezzoConEta);
-    // console.log(prezzoFinale);
-
-
-} else if (utAge >= 65) {
-    // se maggiore o uguale di 65, 40% di sconto
-    const discount = (utPrezzo * 40) / 100;
-    prezzoConEta =  utPrezzo - discount;
-
-
-    //conversione centesimi in euro
-    prezzoFinale = prezzoConEta / 100;
-
-
-    // debug
-    // console.log(discount);
-    // console.log(prezzoConEta);
-    // console.log(prezzoFinale);
-
-
-} else {
-    // se nessuna delle due prezzo base
-    //conversione centesimi in euro
-    prezzoFinale = utPrezzo / 100;
-
-
-    // debug
-    // console.log(prezzoFinale);
-
-
+// clear button
+clear.onclick = function(){
+    document.getElementById("name").value = "";
+    document.getElementById("distance").value = "";
+    document.getElementById ("age").value ="";
 }
 
-// conversione in valuta
-let euro = Intl.NumberFormat('en-DE', {
-    style: 'currency',
-    currency: 'EUR',
-});
 
-toPay = euro.format(prezzoFinale)
+// submit button
+submit.onclick = function(){
 
-
-
-
-// esportazione in html
-document.getElementById ("utName").innerHTML = utName
-document.getElementById ("distance").innerHTML = distance
-document.getElementById ("utAge").innerHTML = utAge
-document.getElementById ("prezzo").innerHTML = toPay
+    // acquisizione info
+    utName = document.getElementById("name").value;
+    distance = document.getElementById("distance").value;
+    utAge = document.getElementById ("age").value;
 
 
+    // calcolo prezzo biglietto, basato sulla distanza, in centesimi
+    utPrezzo = distance * prezzoBase;
 
 
-// main debug
-// console.log(utPrezzo);
-// console.log (distance, utAge);
+    // calcolo prezzo finale basato sull'età
+    if (utAge < 18) {
+        // se minore di 18, 20% di sconto
+        const discount = (utPrezzo * 20) / 100;
+        prezzoConEta =  utPrezzo - discount;
+
+
+        //conversione centesimi in euro
+        prezzoFinale = prezzoConEta / 100;
+
+
+    } else if (utAge >= 65) {
+        // se maggiore o uguale di 65, 40% di sconto
+        const discount = (utPrezzo * 40) / 100;
+        prezzoConEta =  utPrezzo - discount;
+
+
+        //conversione centesimi in euro
+        prezzoFinale = prezzoConEta / 100;
+
+
+    } else {
+        // se nessuna delle due prezzo base
+        //conversione centesimi in euro
+        prezzoFinale = utPrezzo / 100;
+
+
+    }
+
+    // conversione in valuta
+    let euro = Intl.NumberFormat('en-DE', {
+        style: 'currency',
+        currency: 'EUR',
+    });
+
+    toPay = euro.format(prezzoFinale)
+
+
+  
+    document.getElementById("utName").innerText = utName;
+    document.getElementById("finalDistance").innerText = distance;
+    document.getElementById("utAge").innerText = utAge;
+    document.getElementById("toPay").innerText = toPay;
+}
+
+
+
+
 
 
 
